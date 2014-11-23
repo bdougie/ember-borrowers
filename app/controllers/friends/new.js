4e04e1029aac447c64715426168691efe1c4a2a4
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-11-22 22:56:50 -0500

    adds new friend files

---
+++ app/controllers/friends/new.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  actions: {
+    save: function() {
+      console.log('+- save action in friends new contorller');
+
+      return true;
+    },
+    cancel: function() {
+      console.log('+- cancel action in friends new controller');
+
+      return true;
+
+    }
+  }
+});
