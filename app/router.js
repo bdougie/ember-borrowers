cb395d77d3f84dbd17e665bf78b8b456118d3c4c
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-11-23 01:28:52 -0500

    adds edit form

--- app/router.js
+++ app/router.js
@@ -9,8 +9,10 @@ Router.map(function() {
   this.resource('friends', function() {
     this.route('new');
     this.route('show', { path: ':friend_id' });
+    this.route('edit', { path: ':friend_id/edit' });
   });
   this.route('friends/show');
+  this.route('friends/edit');
 });
 
 export default Router;
