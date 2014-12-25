901438512253290a6d0fed319e64f7af1be6047d
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-12-25 06:06:58 -0500

    pg32

--- app/controllers/friends/new.js
+++ app/controllers/friends/new.js
@@ -1,16 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  isValid: Ember.computed(
+    'email',
+    'firstName',
+    'lastName',
+    'twitter',
+    function() {
+      return !Ember.isEmpty(this.get('email')) &&
+        !Ember.isEmpty(this.get('firstName')) &&
+        !Ember.isEmpty(this.get('lastName')) &&
+        !Ember.isEmpty(this.get('twitter'));
+    }
+  ),
   actions: {
     save: function() {
-      console.log('+- save action in friends new controller');
+      if (this.get('isValid')) {
+        var _this = this; this.get('model').save().then(function(friend) {
+          _this.transitionToRoute('friends.show', friend);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
 
-      return true;
+      return false;
     },
     cancel: function() {
-      console.log('+- cancel action in friends new controller');
+      this.transitionToRoute('friends');
 
-      return true;
+      return false;
     }
   }
 });
