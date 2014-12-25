17491307b1ba37d70c27f9b2588373a41abdef58
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-12-25 05:44:20 -0500

    pg29

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,4 +4,14 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
   }
+  //actions: {
+    //save: function() {
+      //console.log('+-- save action bubbled up to friends new route');
+      //return true;
+    //},
+    //cancel: function() {
+      //console.log('+-- cancel action bubbled up to friends new route');
+      //return true;
+    //}
+  //}
 });
