94b97a5dbcbd116ba4283969603d0f3e537b4488
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-12-27 04:05:10 -0500

    fixes router error

--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function() {
     this.route('new');
     this.route('show', { path: ':friend_id' }, function() {
       this.resource('articles', function() { });
-    );
+    });
     this.route('edit', { path: ':friend_id/edit' });
   });
 });
