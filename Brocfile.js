f6acc99b4a1638524faaa257b32817f12296c937
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-12-27 03:15:05 -0500

    pg 50 fonts

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,18 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('bower_components/picnic/releases/v2.min.css');
+app.import('vendor/fontello/fontello.css');
+app.import('vendor/fontello/font/fontello.ttf', {
+  destDir: 'font'
+});
+app.import('vendor/fontello/font/fontello.eot', {
+  destDir: 'font'
+});
+app.import('vendor/fontello/font/fontello.svg', {
+  destDir: 'font'
+});
+app.import('vendor/fontello/font/fontello.woff', {
+  destDir: 'font'
+});
 
 module.exports = app.toTree();
